e069c2a4266ae3e107a750a1a81b4863179d0b90
Caio Adauri <caioadaurigomes@gmail.com>
2021-11-17 13:50:26 -0300

    ajuste app

--- node/index.js
+++ node/index.js
@@ -11,31 +11,45 @@ const config = {
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
+const createTable = `create table if not exists people (id int NOT NULL AUTO_INCREMENT, name varchar(200) NOT NULL, PRIMARY KEY (id))`;
 const sql = `INSERT INTO people(name) values('Caio')`;
 const select = 'SELECT * FROM people where id = 1';
+connection.query(createTable)
 connection.query(sql)
 connection.query(select)
 
 app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<h1>Full Cycle Rocks!</h1>')
-  res.write('<h2>Retorno DB</h2>')
-  res.write(`<div style="background-color: #e3e3e3; font-size: 1.5em;">${JSON.stringify([{ id: 01 }])}</div>`)
-  res.write('<ul style="background-color: #369369; font-size: 1.5em;">')
+
+  const html = `<h1> Full Cycle Rocks! </h1> \
+                <h2> Retorno DB </h2> \
+                <div style="background-color: #e3e3e3; font-size: 1.5em;">${JSON.stringify([{ result }])}</div> \
+                <ul style="background-color: #369369; font-size: 1.5em;"> \
+                </ul>`;
 
   connection.query(select, function (error, results, fields) {
     if(error) {
-      console.log(error);
-      return
+      console.log(error.message);
     }
-    console.log(results)
-    results.forEach(function(row) {
-      console.log(row);
-      res.write(`<li>${row.name}</li`);
-    })
+    const result = results
   })
-  res.write('</ul>')
-  res.end()
+
+
+
+
+  //connection.query(select, function (error, results, fields) {
+  //  if(error) {
+  //    console.log(error);
+  //    return
+  //  }
+  //  console.log(results)
+  //  results.forEach(function(row) {
+  //    console.log(row);
+  //    res.write(`<li>${row.name}</li`);
+  //  })
+  //})
+  //res.write('</ul>')
+  
+  res.send(html);
 })
 
 
